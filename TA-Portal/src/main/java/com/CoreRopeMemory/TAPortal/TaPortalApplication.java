0e75114b840b6a8b949ceffa95d320693293a379
Ali Alladin <ali.alladin20@gmail.com>
2021-04-13 15:19:10 +0200

    Added generated starter-project to the repository.

---
+++ TA-Portal/src/main/java/com/CoreRopeMemory/TAPortal/TaPortalApplication.java
@@ -0,0 +1,13 @@
+package com.CoreRopeMemory.TAPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaPortalApplication.class, args);
+	}
+
+}
