2aa983338e3b2b6a5c43c83d9cf38263d38fb7d6
Ali Alladin <ali.alladin20@gmail.com>
2021-04-16 14:52:05 +0200

    Done with User Story 1.

--- TA-Portal/src/main/java/com/CoreRopeMemory/TAPortal/ApplicationController.java
+++ TA-Portal/src/main/java/com/CoreRopeMemory/TAPortal/ApplicationController.java
@@ -1,16 +1,30 @@
 package com.CoreRopeMemory.TAPortal;
 
+import com.CoreRopeMemory.TAPortal.model.WorkShift;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ApplicationController {
 
     @GetMapping({"/", "/index"})
     public String hello(Model model){
-        model.addAttribute("helloWorld", "Hello World!");
+        model.addAttribute("workshifts", Database.getWorkShifts());
         return "index";
+    }
+
+    @RequestMapping ({"/add_workshift"})
+    public String newWorkshift(Model model){
+        WorkShift workShift = new WorkShift();
+        model.addAttribute("workshift", workShift);
+        return "add_workshift";
+    }
+
+    @RequestMapping(value = {"/save"}, method = RequestMethod.POST)
+    public String addWorkshift(@ModelAttribute ("workshift")WorkShift workShift){
+        Database.addWorkShift(workShift);
+
+        return "redirect:/";
     }
 }
---
+++ TA-Portal/src/main/java/com/CoreRopeMemory/TAPortal/model/WorkShift.java
@@ -0,0 +1,54 @@
+package com.CoreRopeMemory.TAPortal.model;
+
+public class WorkShift {
+    private String date;
+    private String startTime;
+    private String endTime;
+    private String type;
+    private String comment;
+    
+
+    public WorkShift() {
+
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
