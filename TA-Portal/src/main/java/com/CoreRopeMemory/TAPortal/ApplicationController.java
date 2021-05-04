fd4f4b94e9dc8ba7dfde7a6d841f8a6f139162d1
Ali Alladin <ali.alladin20@gmail.com>
2021-05-04 11:38:07 +0200

    Refactored the index page.

--- TA-Portal/src/main/java/com/CoreRopeMemory/TAPortal/ApplicationController.java
+++ TA-Portal/src/main/java/com/CoreRopeMemory/TAPortal/ApplicationController.java
@@ -10,10 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.time.Month;
 
 @Controller
@@ -26,9 +23,9 @@ public class ApplicationController {
     private UserService userService;
 
     @GetMapping({"/", "/index"})
-    public String hello(Model model){
+    public String hello(Model model) {
 
-        if (userService.isEmpty()){
+        if (userService.isEmpty()) {
             User user = new User("123456",
                     "test.person@mail.com",
                     "Person",
@@ -44,7 +41,26 @@ public class ApplicationController {
 
         model.addAttribute("currentUser", userService.get("123456"));
 
-        model.addAttribute("january_workshifts", workshiftService.listByMonth(Month.JANUARY));
+
+        LinkedHashMap<String, List<WorkShift>> months = new LinkedHashMap<>();
+        months.put("JANUARY", workshiftService.listByMonth(Month.JANUARY));
+        months.put("FEBRUARY", workshiftService.listByMonth(Month.FEBRUARY));
+        months.put("MARCH", workshiftService.listByMonth(Month.MARCH));
+        months.put("APRIL", workshiftService.listByMonth(Month.APRIL));
+        months.put("MAY", workshiftService.listByMonth(Month.MAY));
+        months.put("JUNE", workshiftService.listByMonth(Month.JUNE));
+        months.put("JULY", workshiftService.listByMonth(Month.JULY));
+        months.put("AUGUST", workshiftService.listByMonth(Month.AUGUST));
+        months.put("SEPTEMBER", workshiftService.listByMonth(Month.SEPTEMBER));
+        months.put("OCTOBER", workshiftService.listByMonth(Month.OCTOBER));
+        months.put("NOVEMBER", workshiftService.listByMonth(Month.NOVEMBER));
+        months.put("DECEMBER", workshiftService.listByMonth(Month.DECEMBER));
+
+        model.addAttribute("months", months);
+
+
+
+        /*model.addAttribute("january_workshifts", workshiftService.listByMonth(Month.JANUARY));
         model.addAttribute("february_workshifts", workshiftService.listByMonth(Month.FEBRUARY));
         model.addAttribute("march_workshifts", workshiftService.listByMonth(Month.MARCH));
         model.addAttribute("april_workshifts", workshiftService.listByMonth(Month.APRIL));
@@ -55,27 +71,29 @@ public class ApplicationController {
         model.addAttribute("september_workshifts", workshiftService.listByMonth(Month.SEPTEMBER));
         model.addAttribute("october_workshifts", workshiftService.listByMonth(Month.OCTOBER));
         model.addAttribute("november_workshifts", workshiftService.listByMonth(Month.NOVEMBER));
-        model.addAttribute("december_workshifts", workshiftService.listByMonth(Month.DECEMBER));
+        model.addAttribute("december_workshifts", workshiftService.listByMonth(Month.DECEMBER));*/
+
+        model.addAttribute("months", months);
 
         return "index";
     }
 
-    @RequestMapping ({"/add_workshift"})
-    public String newWorkshift(Model model){
+    @RequestMapping({"/add_workshift"})
+    public String newWorkshift(Model model) {
         WorkShift workShift = new WorkShift();
         model.addAttribute("workshift", workShift);
         return "add_workshift";
     }
 
-    @RequestMapping ({"/edit_workshift/{id}"})
-    public String editWorkshift(@PathVariable (value = "id") long id, Model model){
+    @RequestMapping({"/edit_workshift/{id}"})
+    public String editWorkshift(@PathVariable(value = "id") long id, Model model) {
         WorkShift workShift = workshiftService.getWorkshift(id);
         model.addAttribute("workshift", workShift);
         return "edit_workshift";
     }
 
     @RequestMapping(value = {"/save"}, method = RequestMethod.POST)
-    public String addWorkshift(@ModelAttribute ("workshift")WorkShift workShift){
+    public String addWorkshift(@ModelAttribute("workshift") WorkShift workShift) {
         User user = userService.get("123456");
 
         workShift.setTa(user);
@@ -87,7 +105,7 @@ public class ApplicationController {
     }
 
     @RequestMapping(value = {"/edit/{id}"}, method = RequestMethod.POST)
-    public String edit(@ModelAttribute ("workshift")WorkShift workShift, @PathVariable (value = "id") long id) {
+    public String edit(@ModelAttribute("workshift") WorkShift workShift, @PathVariable(value = "id") long id) {
         User user = userService.get("123456");
         workShift.setTa(user);
         workshiftService.save(workShift);
@@ -95,36 +113,40 @@ public class ApplicationController {
     }
 
     @RequestMapping(value = {"/delete_workshift/{id}"})
-    public String delete(@PathVariable (value = "id") long id) {
+    public String delete(@PathVariable(value = "id") long id) {
         workshiftService.delete(id);
         return "redirect:/";
     }
 
-    @RequestMapping ({"/user_details"})
-    public String user(Model model){
+    @RequestMapping({"/user_details"})
+    public String user(Model model) {
         User user = userService.get("123456");
         model.addAttribute("user", user);
         return "user_details";
     }
 
     @RequestMapping(value = {"/saveUser"}, method = RequestMethod.POST)
-    public String saveUserInfo(@ModelAttribute ("user")User user){
+    public String saveUserInfo(@ModelAttribute("user") User user) {
         //Database.saveUserInfo(user);
         userService.save(user);
         return "redirect:/user_details";
     }
 
-    @RequestMapping ({"/time_report"})
-    public String timeReport(Model model){
+    @RequestMapping({"/time_report/{month}"})
+    public String timeReport(@PathVariable("month") Month month, Model model) {
+
+
+        List<WorkShift> workshifts = workshiftService.listByMonth(month);
+
         User user = userService.get("123456");
         model.addAttribute("user", user);
 
-        model.addAttribute("lectureExercises",typeOfWorkshift(user.getWorkshifts(),"Lectures and exercise sessions"));
-        model.addAttribute("supervision",typeOfWorkshift(user.getWorkshifts(),"Project supervision and lab supervision"));
-        model.addAttribute("other",typeOfWorkshift(user.getWorkshifts(),"Other activities"));
-        model.addAttribute("labGrading",typeOfWorkshift(user.getWorkshifts(),"Lab grading"));
+        model.addAttribute("lectureExercises", typeOfWorkshift(workshifts, "Lectures and exercise sessions"));
+        model.addAttribute("supervision", typeOfWorkshift(workshifts, "Project supervision and lab supervision"));
+        model.addAttribute("other", typeOfWorkshift(workshifts, "Other activities"));
+        model.addAttribute("labGrading", typeOfWorkshift(workshifts, "Lab grading"));
         Set<LocalDate> dates = new HashSet<>();
-        for (WorkShift workShift : typeOfWorkshift(user.getWorkshifts(), "Exam grading")){
+        for (WorkShift workShift : typeOfWorkshift(workshifts, "Exam grading")) {
             dates.add(workShift.getDate());
         }
         model.addAttribute("examGrading", dates.size());
@@ -133,14 +155,15 @@ public class ApplicationController {
 
     /**
      * Method that gives a list of a specific type of workshift.
+     *
      * @param workShifts A list of all workshifts.
-     * @param type The name of the type of workshifts wanted.
+     * @param type       The name of the type of workshifts wanted.
      * @return A list of workshifts of a specific type.
      */
-    private List<WorkShift> typeOfWorkshift(List<WorkShift> workShifts, String type){
+    private List<WorkShift> typeOfWorkshift(List<WorkShift> workShifts, String type) {
         List<WorkShift> typeOfWorkshift = new ArrayList<>();
-        for (WorkShift workshift: workShifts) {
-            if (workshift.getType().equals(type)){
+        for (WorkShift workshift : workShifts) {
+            if (workshift.getType().equals(type)) {
                 typeOfWorkshift.add(workshift);
             }
         }
