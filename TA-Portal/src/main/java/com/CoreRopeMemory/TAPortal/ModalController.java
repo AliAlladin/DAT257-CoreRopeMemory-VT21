8121bf71e4fffa762772dc7ca0c7271792736f0b
Samdahl <samdahl@protonmail.com>
2021-05-18 15:53:13 +0200

    edit modal added

--- TA-Portal/src/main/java/com/CoreRopeMemory/TAPortal/ModalController.java
+++ TA-Portal/src/main/java/com/CoreRopeMemory/TAPortal/ModalController.java
@@ -3,6 +3,7 @@ package com.CoreRopeMemory.TAPortal;
 import java.util.List;
 
 import com.CoreRopeMemory.TAPortal.Services.CourseService;
+import com.CoreRopeMemory.TAPortal.Services.WorkshiftService;
 import com.CoreRopeMemory.TAPortal.model.Course;
 import com.CoreRopeMemory.TAPortal.model.WorkShift;
 
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,6 +24,9 @@ public class ModalController {
     @Autowired
     private CourseService courseService;
 
+    @Autowired
+    private WorkshiftService workshiftService;
+
     @GetMapping("modal1")
     public String modal1(Model model) {
         WorkShift workshift = new WorkShift();
@@ -32,8 +37,11 @@ public class ModalController {
     }
 
     @GetMapping("modal2")
-    public String modal2(@RequestParam("name") String name, Model model) {
-        model.addAttribute("name", name);
+    public String modal2(@RequestParam(value = "id") long id, Model model) {
+        WorkShift workShift = workshiftService.getWorkshift(id);
+        model.addAttribute("workshift", workShift);
+        List<Course> courses = courseService.listALl();
+        model.addAttribute("courses", courses);
         return "modal2";
     }
 }
